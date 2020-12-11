9bd3271a0de8054f0fd162667c25a4fcf277dc63
wangshuaiyu <414095706@qq.com>
2020-12-11 14:12:17 +0800

    第一次提交

---
+++ test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by dika on 2020/12/11.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by dika on 2020/12/11.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
